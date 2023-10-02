5d8d5a766bea200cb053d0f792ad47ea40f187e1
anushkaj-01 <122741286+anushkaj-01@users.noreply.github.com>
2023-10-03 01:17:53 +0530

    Faq section added with complete responsiveness and footer with desktop view

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+function faq() {
+  var arrows = document.querySelectorAll('.arrow');
+  var answers=document.querySelectorAll('.answer')
+      for (let answer of answers){
+      answer.style.display = 'none';
+      }
+  for (let arrow of arrows) {
+    arrow.addEventListener('click', function () {
+      
+      var faqContainer = arrow.closest('.faq');
+      
+      var answer = faqContainer.querySelector('.answer');
+      
+      if (answer.style.display === 'none' || answer.style.display === '') {
+        answer.style.display = 'block';
+      } else {
+        answer.style.display = 'none';
+      }
+      arrow.classList.toggle('rotate180');
+    });
+  }
+}
+document.addEventListener('DOMContentLoaded', faq);
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,26 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
+      flex: {
+        '6': '6',
+        '3': '3',
+        '5': '5',
+      },
+      colors:{
+      "black01":"#0F0913",
+      gold1:"#F9CB33",
+      vuejs:"#49e659"
+    },
+    size:{
+      'rl':"200rem",
+      1:"1%",
+      '9/10':"90%"
+    },
+    boxShadow:{
+      custom:" 0 0 1px 1px #FFFBA4"},
+    fontFamily:{
+      Jetbrains: ['JetBrains Mono', 'monospace']
+    },
       fontFamily:{
         "jetbrains":["JetBrains Mono",'monospace']
       },
