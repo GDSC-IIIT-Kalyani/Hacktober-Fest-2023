e79447dff57100552114d3cb06dc1cdff466fcd4
shoeb310 <sshoeb532@gmail.com>
2023-10-10 22:08:33 +0530

    Leaderboard Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
+      
       flex: {
         '6': '6',
         '3': '3',
@@ -19,21 +20,31 @@ module.exports = {
       '9/10':"90%"
     },
     boxShadow:{
-      custom:" 0 0 1px 1px #FFFBA4"},
-    fontFamily:{
-      Jetbrains: ['JetBrains Mono', 'monospace']
+      custom:" 0 0 1px 1px #FFFBA4",
+      'custom1': '9px 9px 3px #ffff',
+      'custom2': '3px 3px 3px #ffff',
     },
-      fontFamily:{
-        "jetbrains":["JetBrains Mono",'monospace']
-      },
-      fontFamily:{
-        "jetbrains":["JetBrains Mono",'monospace']
-      },
+      
+    fontFamily:{
+      Jetbrains: ['JetBrains Mono', 'monospace'],
+      "jetbrains":["JetBrains Mono",'monospace'],
+      'jet_brains' : ['JetBrains Mono'],
+    },
+      // fontFamily:{
+        
+      // },
+      // fontFamily: {
+        
+      // },
       screens:{
         'extrasmall':{"max":'593px'},
         'exsm':{"max":'460px'},
         'verysmall':{'max':"475px"},
         "640tk":{'max':"640px"},
+        'sm_lead': {'min': '640px', 'max': '767px'},
+      'md_lead': {'min': '768px', 'max': '1023px'},
+      'xs_lead': {'min': '30px', 'max': '639px'},
+      'xl_lead': {'min': '1024px', 'max': '1700px'},
       }
     },
   },
